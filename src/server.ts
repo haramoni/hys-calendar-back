bfbf6f3b7ad1302e7671b343f1b34ee9a325c04b
Júlia Haramoni <haramoni.julia@gmail.com>
2026-04-19 15:32:58 -0300

    adicionando https

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,8 @@ async function start() {
   const allowedOrigins = [
     "http://localhost:5173",
     "http://hys-expor-stands.com.br/",
-    "https://localhost:3333",
+    "http://localhost:3333",
+    "https://hys-expor-stands.com.br/",
   ];
 
   await app.register(cors, {
